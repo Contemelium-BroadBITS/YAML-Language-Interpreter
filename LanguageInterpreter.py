6256194ebc034c7995828af80983324adef5d71f
烈星氷刃 <101722929+Contemelia@users.noreply.github.com>
2024-03-05 12:41:38 +0300

    Updated LanguageInterpreter.py | ID: 2024030501
    __processFunction() と __processIntegration() の必要なコードを追加しました。

--- LanguageInterpreter.py
+++ LanguageInterpreter.py
@@ -10,8 +10,12 @@ class YAMLLanguageInterpreter():
     
     def __init__(self, YAMLPayload):
         
-        self.originalPayload = YAMLPayload
-        self.interpretedText = ''
+        self.originalPayload = loadYAML(YAMLPayload)
+        self.interpretedText = ""
+        self.interpretedText += """from re import findall
+from traceback import print_exc
+from yaml import safe_load as loadYAML\n\n\n
+"""
         self.recordIterated = []
         
         self.processes = {
@@ -21,6 +25,8 @@ class YAMLLanguageInterpreter():
             'integration': self.__processIntegration, 
             'module': self.__processModule
         }
+        self.tabsToInclude = 0
+        self.dcitionaryPattern = r'\b(\w+)\[\'(\w+)\'\]'
     
     
     
@@ -35,19 +41,33 @@ class YAMLLanguageInterpreter():
     
     
     def __processFunction(self, payload):
-        ...
+        
+        interpretedText = ""
+        
+        interpretedText += f"{payload.get('name')}({', '.join(payload.get('arguments'))})\n"
+        interpretedText += f"getFunction(\'{payload.get('recordID')}\')\n"
+        
+        return interpretedText
     
     
     
     def __processIntegration(self, payload):
         
-        interpretedTExt = ""
+        interpretedText = ""
+        
+        interpretedText += f"{payload.get('alias')}Original = getIntegration(\'{payload.get('recordID')}\', {payload.get('params')})\n"
+        interpretedText += f"{payload.get('alias')}Copy = {payload.get('alias')}Original[:]"
+        
+        return interpretedText
     
     
     
     def __processModule(self, payload):
         
         interpretedText = ""
+        
+        interpretedText += f"{payload.get('alias')}Original = getModule(\'{payload.get('recordID')}\', {payload.get('params')})\n"
+        interpretedText += f"{payload.get('alias')}Copy = {payload.get('alias')}Original[:]"
     
     
     
@@ -65,13 +85,12 @@ class YAMLLanguageInterpreter():
     
 def initialize():
     
-    with open('languageTestSample.yaml') as stream:
-    anObject = YAMLLanguageInterpreter(stream)
-    stream.close()
-    
+    with open('test.yaml') as stream:
+        anObject = YAMLLanguageInterpreter(stream)
+        stream.close()
 
-anObject.processPayload()
-print(anObject.interpretedText)
+    anObject.processPayload()
+    print(anObject.interpretedText)
 
 
 
